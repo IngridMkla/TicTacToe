f21f9e4824348bde138f6e584da5a52d2c450243
IngridMkla <ingrid.moukala@livecolonies.com>
2024-10-14 09:48:32 +0200

    game update

--- game.rb
+++ game.rb
@@ -1,3 +1,23 @@
 class Game
+  @@current_player
+  @@winner
+  attr_reader :player1, :player2
+  attr_accessor :board
+  def initialize()
 
+    @board = Board.new
+  end
+  def start
+    begin
+      #on creer les joueurs
+      puts "Joueur 1, saisiseez votre nom: "
+      joueur1 = gets.chomp
+      @player1 = Player.new(joueur1)
+
+      puts "Joueur 2, saisiseez votre nom: "
+      joueur2 = gets.chomp
+      @player2 = Player.new(joueur2)
+
+    end while is_Full?
+  end
 end
